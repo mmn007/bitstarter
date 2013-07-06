17f0c32abe323fe224946794044141e52477f1f4
MMN <mmn007@gmail.com>
2013-07-06 18:50:00 +0530

    URL support

--- grader.js
+++ grader.js
@@ -27,6 +27,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = 'checks.json';
+var URL_DEFAULT = 'checks.json';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -64,7 +65,8 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        //.option('-f, --file', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url', 'URL Path', URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
