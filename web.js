20e995b9db12c84907d421a542972a2f7b69ae44
MMN <mmn007@gmail.com>
2013-07-06 08:35:58 +0530

    Bugfix

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var buf = new Buffer(fs.readFileSync('index.html'));
-    response.send(buf.toString()w);
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
